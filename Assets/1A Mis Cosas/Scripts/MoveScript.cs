306c04cd5108d7c46e19ca62067fd5d896b4b614
MaxwellTav <72995619+MaxwellTav@users.noreply.github.com>
2020-10-26 13:03:51 -0400

    subiendo el proyecto
    subiendo el proyecto

---
+++ Assets/1A Mis Cosas/Scripts/MoveScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    //Variables
+    public GameObject _ObjectoParaRotar;
+    public int speedTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _ObjectoParaRotar.transform.Rotate(0, Time.deltaTime * speedTime, 0, Space.Self);
+    }
+}
